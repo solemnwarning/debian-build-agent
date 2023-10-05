c2e4ae9da5bfed01c8c4d28cc9cde1790100d853
Daniel Collins <solemnwarning@solemnwarning.net>
2023-10-05 23:17:27 +0100

    Add remote deployment scripts.

---
+++ remote-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HOST="root@vmhost01.lan.solemnwarning.net"
+DIR="/mnt/vmbuild/debian-build-agent"
+
+set -e
+
+cd "$(dirname "$0")"
+
+rsync -tpr --delete --exclude-from=.gitignore -e ssh . "$HOST:$DIR"
+ssh "$HOST" "$DIR/build.sh"
